a7f030024ea8ab9dc479d0bb217f653ba0a0c49d
Ershov-VA <ershov_va@bk.ru>
2023-01-01 14:24:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-e1byvi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-e1byvi)
---
+++ index.ts
@@ -0,0 +1,29 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+interface IVoyantType {
+  type: string;
+}
+
+interface IVoyant {
+  name: string;
+  id: number;
+  type: IVoyantType[];
+}
+
+const voyants: IVoyant[] = [
+  { id: 1, name: 'name 1', type: [{ type: 'Astrology' }] },
+  { id: 2, name: 'name 2', type: [{ type: 'Astrology' }] },
+];
+
+function getVoyantById(id: number): IVoyant | null {
+  return voyants.find((voyant) => voyant.id === id) || null;
+}
+
+const voyantDiv: HTMLElement = document.querySelector('#voyant');
+
+voyantDiv.innerHTML = `${getVoyantById(1).name}`;
